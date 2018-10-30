45cc8beddf6ae7ebc9723abae9cc91db5d5dc195
alexlivenson <alexlivenson@gmail.com>
2018-10-29 17:35:02 -0700

    Alex L - Updated deps

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Button, Button2, Link } from "../core/";
+import { Header, Button, Link } from "../core/";
 import "./custom.scss";
 
 class App extends Component {
@@ -9,7 +9,6 @@ class App extends Component {
                 <Header> This is cool yes! Momey </Header>
                 <Button>This is an example of a regular button</Button>
                 <Button variation={Button.Styles.primary}>primary</Button>
-                <Button2 variation={Button.Styles.primary}>BUTTON 2</Button2>
                 <Button variation={Button.Styles.secondary}>secondary</Button>
                 <Button className="bigger-button">Using another class</Button>
                 <Link>Link</Link>
--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header/Header";
 export { default as Button } from "./elements/Button/Button";
-export { default as Button2 } from "./elements/Button2/Button2";
 export { default as Link } from "./elements/Link";
